fe773c06e994c7093c3fbfcf0caadb243ed4c940
rahulg3520 <rahul@nestgroup.net>
2022-12-12 11:57:55 +0530

    backend for nest digital app

---
+++ src/main/java/com/example/Nest_Digital_App_backend/NestDigitalAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nest_Digital_App_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigitalAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigitalAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Nest_Digital_App_backend/NestDigitalAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nest_Digital_App_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestDigitalAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
